bdf8804c5d8177c38e14eb2e1e32f90613dc1444
kkumong9@gmail.com <kkumong9@gmail.com>
2019-07-31 16:37:11 +0900

    DrivingMode and ReportMovement

---
+++ src/api/User/ReportMovement/ReportMovement.resolvers.ts
@@ -0,0 +1,37 @@
+import User from "../../../entities/User";
+import {
+  ReportMovementMutationArgs,
+  ReportMovementResponse
+} from "../../../types/graph";
+import { Resolvers } from "../../../types/resolvers";
+import cleanNullArgs from "../../../utils/cleanNullArg";
+import privateResolver from "../../../utils/privateResolver";
+
+const resolvers: Resolvers = {
+  Mutation: {
+    ReportMovementResponse: privateResolver(
+      async (
+        _,
+        args: ReportMovementMutationArgs,
+        { req }
+      ): Promise<ReportMovementResponse> => {
+        const user: User = req.user;
+        const notNull: any = cleanNullArgs(args);
+        try {
+          await User.update({ id: user.id }, { ...notNull });
+          return {
+            ok: true,
+            error: null
+          };
+        } catch (error) {
+          return {
+            ok: false,
+            error: error.message
+          };
+        }
+      }
+    )
+  }
+};
+
+export default resolvers;
